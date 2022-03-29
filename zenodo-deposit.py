a79fe58b6e70c3d6ebcc77fb3a54f61267065936
Antonio Calanducci <antonio.calanducci@gmail.com>
2022-03-29 15:35:09 +0200

    add file existance check otherwise fails

--- zenodo-deposit.py
+++ zenodo-deposit.py
@@ -3,6 +3,7 @@ import yaml
 import sys
 import urllib.parse
 import json
+from os.path import exists as file_exists
 
 # Choose a target host
 oar_host = "www.openaccessrepository.it"
@@ -25,15 +26,42 @@ if len(sys.argv) < 2:
     print("please provide a file .yaml to read records' metadata")
     exit(-1)
 yaml_file = sys.argv[1]
-with open(yaml_file) as file:
-    # The FullLoader parameter handles the conversion from YAML
-    # scalar values to Python the dictionary format
-    records = yaml.load(file, Loader=yaml.FullLoader)
+
+try:
+    with open(yaml_file) as file:
+        # The FullLoader parameter handles the conversion from YAML
+        # scalar values to Python the dictionary format
+        records = yaml.load(file, Loader=yaml.FullLoader)
+except:
+    print("Cannot read ", yaml_file)
+    exit(-1)
 
 if len(records) == 0:
     print("no records found")
     exit(-1)
 print(f"Found {len(records)} records")
+
+
+# se non viene passato alcun indice viene caricato il primo record del file .yaml
+if len(sys.argv) == 3:
+    r = records[int(sys.argv[2])]
+else:
+    r = records[0]
+
+
+# Costruiamo il nome del file su OAR in base al titolo ed al path del record
+
+filename = r['title'].replace(' ', '-').lower()
+filename += '_' + r['files'][0]['path'].replace('/', '_')
+filename = urllib.parse.quote(filename)
+path = r['files'][0]['path']
+print(f"\nUploading '{r['title']}' as {filename}")
+
+
+# Check if file exists
+if not file_exists(path):
+    print(f"{path} doesn't exists")
+    exit(-1)
 
 
 api = "/api/deposit/depositions"
@@ -60,38 +88,31 @@ print(f"Preserved DOI {reserved_doi} for deposition {deposition_id}")
 bucket_url = req.json()["links"]["bucket"]
 
 
-# se non viene passato alcun indice viene caricato il primo record del file .yaml
-if len(sys.argv) == 3:
-    r = records[sys.argv[2]]
-else:
-    r = records[0]
-
-# Costruiamo il nome del file su OAR in base al titolo ed al path del record
-
-filename = r['title'].replace(' ', '-').lower()
-filename += '_' + r['files'][0]['path'].replace('/', '_')
-filename = urllib.parse.quote(filename)
-path = r['files'][0]['path']
 upload_url = f'{bucket_url}/{filename}'
-print("Upload to: ", upload_url)
+print(f"Upload to '{upload_url}'")
+
 
 # Let's upload the first file in the files attributes
 # The target URL is a combination of the bucket link with the desired filename
 # seperated by a slash.
-with open(path, "rb") as fp:
-    req = requests.put(
-        upload_url,
-        data=fp,
-        params=params,
-    )
+try:
+    with open(path, "rb") as fp:
+        req = requests.put(
+            upload_url,
+            data=fp,
+            params=params,
+        )
+except:
+    print("Error trying to read the file from ", path)
+    exit(-1)
 
 # TODO: controllare se upload termina con successo
 
 # eliminiamo la sezione files dagli attributi
 del r['files']
 
-data = {'metadata': r}
-print("metadata", data)
+data = {'Metadata': r}
+print("metadata: \n", yaml.dump(r))
 
 
 url = f'{deposit_url}/{deposition_id}'
